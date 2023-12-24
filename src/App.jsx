8676ffac3f43e3944caf4b8e104de5cebd9a0ff9
amidamarudr <dev.amidamaru@mail.ru>
2023-12-24 22:10:00 +0300

    make footer and header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+ 
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
