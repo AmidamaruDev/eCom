5b87a60938d479b40b0ab9074068fb52d587c988
amidamarudr <dev.amidamaru@gmail.com>
2023-12-25 18:24:04 +0300

    add card items and array methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,66 @@
+import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Items from './components/Items';
  
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpeg',
+          desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, voluptatibus.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.webp',
+          desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, voluptatibus.',
+          category: 'chairs',
+          price: '149.99'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, voluptatibus.',
+          category: 'chairs',
+          price: '249.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'wall-light.jpeg',
+          desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, voluptatibus.',
+          category: 'light',
+          price: '29.99'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpeg',
+          desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, voluptatibus.',
+          category: 'chairs',
+          price: '49.99'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
